41cac525bf916fbba7c668461a6d433ea81de01b
yindaheng98 <yindaheng98@gmail.com>
2023-12-06 16:37:23 -0800

    send welcome msg

--- citation_rabbot/__main__.py
+++ citation_rabbot/__main__.py
@@ -1,7 +1,7 @@
 import argparse
 import logging
 from neo4j import GraphDatabase
-from telegram.ext import ApplicationBuilder
+from telegram.ext import CommandHandler, ApplicationBuilder
 from .arg import add_argument_jump, parse_args_jump
 from .jump import Rabbot
 logging.basicConfig(
@@ -20,6 +20,13 @@ add_argument_jump(parser)
 args = parser.parse_args()
 jump_list = parse_args_jump(parser)
 
+
+async def start(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a rabbit jumping in citation database")
+
 application = ApplicationBuilder().token(args.token).build()
+application.add_handler(CommandHandler('start', start))
 with GraphDatabase.driver(args.uri, auth=args.auth) as driver:
-    rabbot = Rabbot(bot=application)
+    with driver.session() as session:
+        rabbot = Rabbot(bot=application, session=session)
+        application.run_polling()
